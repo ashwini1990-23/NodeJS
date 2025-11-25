a8e28e95861f8d6a6c3fa69b9553781f1f295629
Ashwini <ashwiniaski23@gmail.com>
2025-11-25 15:49:25 -0500

    Adding creatin webserver, api and Routing code

--- 01-Node-Farm/index.js
+++ 01-Node-Farm/index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
-
+const http = require("http");
+const url = require("url");
+////////////////////////////////////////////
+// Files
+/*
 // Blocking / Synchronous way
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
@@ -17,10 +21,49 @@ fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
     fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
       console.log(data3);
 
-      fs.writeFile(".txt/final.txt", `${data2}\n${data3}`, "utf-8", (error) => {
-        console.log("File has been written.😁");
-      });
+      fs.writeFile(
+        ".txt/final.txt",
+        `${data2} \n${data3}`,
+        "utf-8",
+        (error) => {
+          console.log("File has been written.😁");
+        }
+      );
     });
   });
 });
 console.log("will read file.......");
+*/
+
+////////////////////////////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-wolrd",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
+
+///////////////////////////////////
+// Building simple Web API
