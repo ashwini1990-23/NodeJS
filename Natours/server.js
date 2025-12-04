a019e5d862a193073ba770fcb420e92f46828913
Ashwini <ashwiniaski23@gmail.com>
2025-12-04 15:34:33 -0500

    Code for remote Databse connection

--- Natours/server.js
+++ Natours/server.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-console.log(app.get('env'));
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // console.log(con.connections);
+    console.log(`DB connection successful`);
+  });
+
+// console.log(app.get('env'));
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
