e53cff0e6470f382d914e8f91050595e21cc0b16
Ashwini <ashwiniaski23@gmail.com>
2026-01-05 15:56:43 -0500

    code for routing, content-type,route not found and path parameters

--- Wild-Horizons/server.js
+++ Wild-Horizons/server.js
@@ -1,11 +1,37 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  res.write("This is some data \n");
+const server = http.createServer(async (req, res) => {
+  /*res.write("This is some data \n");
   res.write("This is some more data \n");
-  res.end("Hello from the server!", "utf8", () => console.log("response end"));
+  res.end("Hello from the server!", "utf8", () => console.log("response end"));*/
+
+  const destinations = await getDataFromDB();
+
+  if (req.url === "/api" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(destinations));
+  } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
+    const continent = req.url.split("/").pop();
+    const filteredData = destinations.filter((destination) => {
+      return destination.continent.toLowerCase() === continent.toLowerCase();
+    });
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(filteredData));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        error: "not found",
+        message: "The requested route does not exist",
+      })
+    );
+  }
 });
 
 server.listen(PORT, () => {
