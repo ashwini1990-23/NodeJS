c94128718482bc94ec5e2202e4659307898ada8a
Ashwini <ashwiniaski23@gmail.com>
2026-01-15 13:03:36 -0500

    SignUp,Login,Logout frontend files modified and backend Auth functionality added

--- Spiral-Sounds/server.js
+++ Spiral-Sounds/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 import { productsRouter } from "./routes/products.js";
+import { authRouter } from "./routes/auth.js";
 
 const PORT = 8000;
 
 const app = express();
 
+// Parsing incoming JSON and putting results in req.body
+app.use(express.json());
+
 app.use(express.static("public"));
 
 app.use("/api/products", productsRouter);
+app.use("/api/auth", authRouter);
 
 app
   .listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
