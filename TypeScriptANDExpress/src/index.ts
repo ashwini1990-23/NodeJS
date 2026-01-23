0c58dc77b3533ebb9305173f1a71b079428d33e2
Ashwini <ashwiniaski23@gmail.com>
2026-01-22 20:59:33 -0500

    First code commit for typescript and express project

---
+++ TypeScriptANDExpress/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type { Express, Request, Response } from "express";
+import cors from "cors";
+
+import { pets } from "./data/pets";
+
+const PORT = 8000;
+
+const app: Express = express();
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response): void => {
+  res.json(pets);
+});
+
+app.listen(PORT, (): void => console.log(`Listening on port: ${PORT}`));
