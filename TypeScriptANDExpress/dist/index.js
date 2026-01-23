ea59a3c1c0af9281f2552e58721aefaa9d20fc3e
Ashwini <ashwiniaski23@gmail.com>
2026-01-23 15:45:24 -0500

    Typing path params and query params in express

--- TypeScriptANDExpress/dist/index.js
+++ TypeScriptANDExpress/dist/index.js
@@ -10,6 +10,24 @@ const PORT = 8000;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
-    res.json(pets_1.pets);
+    const { species } = req.query;
+    let filteredPets = pets_1.pets;
+    if (species) {
+        filteredPets = filteredPets.filter((pet) => pet.species.toLowerCase() === species.toLowerCase());
+    }
+    res.json(filteredPets);
+});
+app.get("/:id", (req, res) => {
+    let { id } = req.params;
+    const pet = pets_1.pets.find((pet) => pet.id.toString() === id);
+    if (pet) {
+        res.json(pet);
+    }
+    else {
+        res.status(404).json({ message: "No pet with that ID" });
+    }
+});
+app.use((req, res) => {
+    res.status(404).json({ message: "Endpoint not found" });
 });
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
--- TypeScriptANDExpress/src/index.ts
+++ TypeScriptANDExpress/src/index.ts
@@ -3,6 +3,7 @@ import type { Express, Request, Response } from "express";
 import cors from "cors";
 
 import { pets } from "./data/pets";
+import type { Pet } from "./data/pets";
 
 const PORT = 8000;
 
@@ -10,8 +11,46 @@ const app: Express = express();
 
 app.use(cors());
 
-app.get("/", (req: Request, res: Response): void => {
-  res.json(pets);
+app.get(
+  "/",
+  (
+    req: Request<{}, unknown, {}, { species?: string }>,
+    res: Response<Pet[]>,
+  ): void => {
+    const { species } = req.query;
+
+    let filteredPets: Pet[] = pets;
+
+    if (species) {
+      filteredPets = filteredPets.filter(
+        (pet: Pet): boolean =>
+          pet.species.toLowerCase() === species.toLowerCase(),
+      );
+    }
+    res.json(filteredPets);
+  },
+);
+
+app.get(
+  "/:id",
+  (
+    req: Request<{ id: string }>,
+    res: Response<Pet | { message: string }>,
+  ): void => {
+    let { id } = req.params;
+    const pet: Pet | undefined = pets.find(
+      (pet: Pet): boolean => pet.id.toString() === id,
+    );
+    if (pet) {
+      res.json(pet);
+    } else {
+      res.status(404).json({ message: "No pet with that ID" });
+    }
+  },
+);
+
+app.use((req: Request, res: Response): void => {
+  res.status(404).json({ message: "Endpoint not found" });
 });
 
 app.listen(PORT, (): void => console.log(`Listening on port: ${PORT}`));
