eb9e1b5ee9039f2f3c154224fc80e622f7c074f3
Ashwini <ashwiniaski23@gmail.com>
2026-02-01 15:30:39 -0500

    Adding error handling and exress.json() parser code

--- ContactManagementApp/controllers/contactController.js
+++ ContactManagementApp/controllers/contactController.js
@@ -1,28 +1,34 @@
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-export function getAllContacts(req, res) {
+export async function getAllContacts(req, res) {
   res.status(200).json({ message: "Get all Contacts" });
 }
 
 //@desc Create new contact
 //@route POST /api/contacts
 //@access public
-export function createContact(req, res) {
+export async function createContact(req, res) {
+  console.log("The req body:", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
   res.status(201).json({ message: "Create contact" });
 }
 
 //@desc Get contact based on id
 //@route GET /api/contacts/:id
 //@access public
-export function getContact(req, res) {
+export async function getContact(req, res) {
   res.status(200).json({ message: `Get contact based on id ${req.params.id}` });
 }
 
 //@desc Update contact based on id
 //@route PUT /api/contacts/:id
 //@access public
-export function updateContactById(req, res) {
+export async function updateContactById(req, res) {
   res
     .status(200)
     .json({ message: `Update contact based on id ${req.params.id}` });
@@ -31,7 +37,7 @@ export function updateContactById(req, res) {
 //@desc Delete contact based on id
 //@route DELETE /api/contacts/:id
 //@access public
-export function deleteContact(req, res) {
+export async function deleteContact(req, res) {
   res
     .status(200)
     .json({ message: `Delete contact based on id ${req.params.id}` });
--- ContactManagementApp/server.js
+++ ContactManagementApp/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { contactRouter } from "./routes/contactRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", contactRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
